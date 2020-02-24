3deb5891cf2f2230a4f44c46771095cd5008011d
Aman Tiwari <amant5186@gmail.com>
2020-02-24 17:26:39 +0530

    Fix: Changed the message displayed after the task is added

--- ToDoList.py
+++ ToDoList.py
@@ -1,2 +1,9 @@
+tasks = []
+task_number = 0
+
+
 def add(task):
-    return "Task Added Successfully"
+    global task_number
+    task_number += 1
+    task1 = "Task " + str(task_number) + ": " + task + "\n"
+    return task1
--- ToDoListTest.py
+++ ToDoListTest.py
@@ -5,7 +5,10 @@ import ToDoList
 
 class ToDoListTest(unittest.TestCase):
     def test_adding_first_task_adds_the_task_to_the_list(self):
-        self.assertEqual("Task Added Successfully", ToDoList.add("Hi"))
+        self.assertEqual("Task 1: Hi\n", ToDoList.add("Hi"))
+
+    def test_adding_second_task_adds_the_task_to_the_list(self):
+        self.assertEqual("Task 2: Hello\n", ToDoList.add("Hello"))
 
 
 if __name__ == '__main__':
