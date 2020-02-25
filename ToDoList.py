e4195cd090c9686eccef43e68552710c6fac9b4f
Aman Tiwari <amant5186@gmail.com>
2020-02-25 10:00:05 +0530

    Add: Added a class for todo list

--- ToDoList.py
+++ ToDoList.py
@@ -1,9 +1,14 @@
-tasks = []
-task_number = 0
+class ToDoList:
+    def __init__(self):
+        self.tasks = []
+        self.task_number = 0
 
+    def add(self):
+        task = input("Enter the task: ")
+        self.task_number += 1
+        to_do = "Task " + str(self.task_number) + ": " + task
+        self.tasks.append(to_do)
 
-def add(task):
-    global task_number
-    task_number += 1
-    task1 = "Task " + str(task_number) + ": " + task + "\n"
-    return task1
+    def view(self):
+        for task in self.tasks:
+            print(task)
--- ToDoListTest.py
+++ ToDoListTest.py
@@ -1,14 +1,28 @@
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from ToDoList import ToDoList
 
-import ToDoList
+task = ToDoList()
 
 
 class ToDoListTest(unittest.TestCase):
-    def test_adding_first_task_adds_the_task_to_the_list(self):
-        self.assertEqual("Task 1: Hi\n", ToDoList.add("Hi"))
+    input_string_for_first_test = "Hi"
 
-    def test_adding_second_task_adds_the_task_to_the_list(self):
-        self.assertEqual("Task 2: Hello\n", ToDoList.add("Hello"))
+    # ToDo - Its just sample test that is working and class has been created need to work through for further tests
+    @patch('builtins.input', return_value=input_string_for_first_test)
+    def test_adding_first_task_adds_the_task_to_the_list(self, mock_input):
+        task.add()
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            task.view()
+            self.assertEqual("Task 1: Hi\n", fake_out.getvalue())
+
+    input_string_for_second_test = "Hello"
+    #
+    # @patch('builtins.input', return_value=input_string_for_first_test)
+    # def test_adding_second_task_adds_the_task_to_the_list(self, mock_input):
+    #     ToDoList().add()
+    #     self.assertEqual("Task 1: Hi\nTask 2: Hello", ToDoList().view())
 
 
 if __name__ == '__main__':
