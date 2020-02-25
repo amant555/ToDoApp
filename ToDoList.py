256e85da43f3ee0da359797ecf6a752a5e4b7dec
Aman Tiwari <amant5186@gmail.com>
2020-02-25 11:43:40 +0530

    Temp commit

--- ToDoList.py
+++ ToDoList.py
@@ -1,13 +1,12 @@
 class ToDoList:
     def __init__(self):
         self.tasks = []
-        self.task_number = 0
 
     def add(self):
         task = input("Enter the task: ")
-        self.task_number += 1
-        to_do = "Task " + str(self.task_number) + ": " + task
-        self.tasks.append(to_do)
+        if len(task) == 0:
+            raise ValueError("Empty Task")
+        self.tasks.append(task)
 
     def view(self):
         for task in self.tasks:
--- ToDoListTest.py
+++ ToDoListTest.py
@@ -3,26 +3,53 @@ from unittest.mock import patch
 from io import StringIO
 from ToDoList import ToDoList
 
-task = ToDoList()
-
 
 class ToDoListTest(unittest.TestCase):
-    input_string_for_first_test = "Hi"
+    input_string_for_first_test = "Meet Ema at 7"
 
-    # ToDo - Its just sample test that is working and class has been created need to work through for further tests
     @patch('builtins.input', return_value=input_string_for_first_test)
     def test_adding_first_task_adds_the_task_to_the_list(self, mock_input):
+        task = ToDoList()
         task.add()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             task.view()
-            self.assertEqual("Task 1: Hi\n", fake_out.getvalue())
+            self.assertEqual("Meet Ema at 7\n", fake_out.getvalue())
 
-    input_string_for_second_test = "Hello"
+    input_string_1_for_second_test = "Meet Ema at 7"
+    input_string_2_for_second_test = "Complete the assignment"
+
+    @patch('builtins.input', side_effect=[input_string_1_for_second_test, input_string_2_for_second_test])
+    def test_adding_second_task_adds_the_task_to_the_list(self, mock_input):
+        task = ToDoList()
+        task.add()
+        task.add()
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            task.view()
+            self.assertEqual("Meet Ema at 7\nComplete the assignment\n", fake_out.getvalue())
+
+    input_string_for_third_test = ''
+
+    @patch('builtins.input', return_value=input_string_for_third_test)
+    def test_adding_no_task_raises_value_error(self, mock_input):
+        task = ToDoList()
+        with self.assertRaisesWithMessage(ValueError):
+            task.add()
+
+    # input_string_1_for_second_test = "Meet Ema at 7"
+    # input_string_2_for_second_test = "Complete the assignment"
     #
-    # @patch('builtins.input', return_value=input_string_for_first_test)
-    # def test_adding_second_task_adds_the_task_to_the_list(self, mock_input):
-    #     ToDoList().add()
-    #     self.assertEqual("Task 1: Hi\nTask 2: Hello", ToDoList().view())
+    # @patch('builtins.input', side_effect=[input_string_1_for_second_test, input_string_2_for_second_test])
+    # def test_marking_task_as_complete_and_seperating_it_from_pending_tasks(self, mock_input):
+    #     task = ToDoList()
+    #     task.add()
+    #     task.add()
+    #     task.complete()
+    #     with patch('sys.stdout', new=StringIO()) as fake_out:
+    #         task.view()
+    #         self.assertEqual("Complete the assignment\n", fake_out.getvalue())
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
 
 
 if __name__ == '__main__':
