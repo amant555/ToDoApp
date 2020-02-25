26d9483bde8985c6d80e848af213efd5df04e0c6
Aman Tiwari <amant5186@gmail.com>
2020-02-25 13:11:44 +0530

    Refactor: refactored method names

--- ToDoList.py
+++ ToDoList.py
@@ -2,12 +2,12 @@ class ToDoList:
     def __init__(self):
         self.tasks = []
 
-    def addtask(self):
+    def add_task(self):
         task = input("Enter the task: ")
         if len(task) == 0:
             raise ValueError("Empty Task")
         self.tasks.append(task)
 
-    def viewtasks(self):
+    def view_tasks(self):
         for task in self.tasks:
             print(task)
--- ToDoListTest.py
+++ ToDoListTest.py
@@ -8,18 +8,18 @@ class ToDoListTest(unittest.TestCase):
     @patch('builtins.input', return_value="Meet Ema at 7")
     def test_adding_single_task_in_todo_and_matching_view_output_with_expected_for_single_task(self, mock_input):
         todo = ToDoList()
-        todo.addtask()
+        todo.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
-            todo.viewtasks()
+            todo.view_tasks()
             self.assertEqual("Meet Ema at 7\n", fake_out.getvalue())
 
     @patch('builtins.input', side_effect=["Meet Ema at 7", "Complete the assignment"])
     def test_adding_two_tasks_in_todo_and_checking_if_view_output_matches_expected(self, mock_input):
         todo = ToDoList()
-        todo.addtask()
-        todo.addtask()
+        todo.add_task()
+        todo.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
-            todo.viewtasks()
+            todo.view_tasks()
             self.assertEqual("Meet Ema at 7\nComplete the assignment\n", fake_out.getvalue())
 
     input_string_for_third_test = ''
@@ -28,7 +28,7 @@ class ToDoListTest(unittest.TestCase):
     def test_adding_no_task_raises_value_error(self, mock_input):
         todo = ToDoList()
         with self.assertRaisesRegex(ValueError, "Empty Task"):
-            todo.addtask()
+            todo.add_task()
 
 
 if __name__ == '__main__':
