d76ef63d7d24cae486e4d772384734fa0cbd0896
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2020-02-25 16:56:24 +0530

    Rishabh/Astha Fix:Implementation of view method

--- ToDoList.py
+++ ToDoList.py
@@ -1,26 +1,54 @@
 class ToDoList:
     def __init__(self):
-        self.tasks = []
+        self.incomplete_tasks = []
+        self.completed_tasks = []
 
     def add_task(self):
         task = input("Enter the task: ")
         if len(task) == 0:
             raise ValueError("Empty Task")
-        self.tasks.append(task)
+        self.incomplete_tasks.append(task)
 
     def view_tasks(self):
-        if self.tasks:
+        if len(self.incomplete_tasks) == 0 and len(self.completed_tasks) == 0:
+            print("Your TODO list is empty!")
+
+        elif len(self.incomplete_tasks) > 0 and len(self.completed_tasks) == 0:
+            print("Incomplete Tasks:\n")
             task_number = 1
-            for task in self.tasks:
+            for task in self.incomplete_tasks:
                 print(str(task_number) + ". " + task)
                 task_number += 1
-        else:
-            print("Your TODO list is empty!")
+        elif len(self.completed_tasks) > 0 and len(self.incomplete_tasks) == 0:
+            print("Complete Tasks:\n")
+            task_number = 1
+            for task in self.completed_tasks:
+                print(str(task_number) + ". " + task)
+                task_number += 1
+
+        elif len(self.completed_tasks) > 0 and len(self.incomplete_tasks) > 0:
+            print("Incomplete Tasks:\n")
+            task_number = 1
+            for task in self.incomplete_tasks:
+                print(str(task_number) + ". " + task)
+                task_number += 1
+
+            print("Complete Tasks: \n")
+            task_number = 1
+            for task in self.completed_tasks:
+                print(str(task_number) + ". " + task)
+                task_number += 1
 
     def edit_task(self):
         task_to_edit = int(input("Enter the task number to edit (like 1 for first task you added) : "))
-        if task_to_edit < 1 or task_to_edit > len(self.tasks):
+        if task_to_edit < 1 or task_to_edit > len(self.incomplete_tasks):
             raise ValueError("Task number invalid")
         task_to_edit -= 1
         updated_task = input("Enter the updated task you want in place of previous one : ")
-        self.tasks[task_to_edit] = updated_task
+        self.incomplete_tasks[task_to_edit] = updated_task
+
+    def mark_completed(self):
+        self.view_tasks()
+        task_number = input("Enter the task number of task to be marked as completed: ")
+        self.completed_tasks.append(self.incomplete_tasks[task_number - 1])
+        del self.incomplete_tasks[task_number - 1]
--- ToDoListTest.py
+++ ToDoListTest.py
@@ -11,7 +11,7 @@ class ToDoListTest(unittest.TestCase):
         todo.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Meet Ema at 7\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Meet Ema at 7\n", fake_out.getvalue())
 
     @patch('builtins.input', side_effect=["Meet Ema at 7", "Complete the assignment"])
     def test_adding_two_tasks_in_todo_and_checking_if_view_output_matches_expected(self, mock_input):
@@ -20,7 +20,7 @@ class ToDoListTest(unittest.TestCase):
         todo.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Meet Ema at 7\n2. Complete the assignment\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Meet Ema at 7\n2. Complete the assignment\n", fake_out.getvalue())
 
     @patch('builtins.input', side_effect=["Meet Ema at 7", "Complete the assignment"])
     def test_adding_two_tasks_in_two_different_todo_lists_and_checking_if_view_output_matches_expected(self, mock_input):
@@ -30,7 +30,7 @@ class ToDoListTest(unittest.TestCase):
         todo1.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Meet Ema at 7\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Meet Ema at 7\n", fake_out.getvalue())
 
     @patch('builtins.input', return_value='')
     def test_adding_no_task_raises_value_error(self, mock_input):
@@ -38,18 +38,17 @@ class ToDoListTest(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "Empty Task"):
             todo.add_task()
 
-
     @patch('builtins.input', side_effect=["Meet Ema at 7:00", 1, "Meet Ema at 8:00"])
     def test_adding_task_and_editing_there_itself(self, mock_input):
         todo = ToDoList()
         todo.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Meet Ema at 7:00\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Meet Ema at 7:00\n", fake_out.getvalue())
         todo.edit_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Meet Ema at 8:00\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Meet Ema at 8:00\n", fake_out.getvalue())
 
     def test_empty_todo_list(self):
         todo = ToDoList()
@@ -57,18 +56,19 @@ class ToDoListTest(unittest.TestCase):
             todo.view_tasks()
             self.assertEqual("Your TODO list is empty!\n", fake_out.getvalue())
 
-    @patch('builtins.input', side_effect=["Have Lunch at 1:00pm","Meet Ema at 7:00", 2, "Meet Stacy at 8:00"])
+    @patch('builtins.input', side_effect=["Have Lunch at 1:00pm", "Meet Ema at 7:00", 2, "Meet Stacy at 8:00"])
     def test_adding_two_task_and_editing_second_task(self, mock_input):
         todo = ToDoList()
         todo.add_task()
         todo.add_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Have Lunch at 1:00pm\n2. Meet Ema at 7:00\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Have Lunch at 1:00pm\n2. Meet Ema at 7:00\n", fake_out.getvalue())
         todo.edit_task()
         with patch('sys.stdout', new=StringIO()) as fake_out:
             todo.view_tasks()
-            self.assertEqual("1. Have Lunch at 1:00pm\n2. Meet Stacy at 8:00\n", fake_out.getvalue())
+            self.assertEqual("Incomplete Tasks:\n\n1. Have Lunch at 1:00pm\n2. Meet Stacy at 8:00\n", fake_out.getvalue())
+
 
 if __name__ == '__main__':
     unittest.main()
