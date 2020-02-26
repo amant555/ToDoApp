5209bdef9b59f2e63823de4e3a7e8675391ab3ad
ps <priyanshi184@gmail.com>
2020-02-26 10:45:45 +0530

    Priyanshi|Astha:Added functionality for empty task in edit

--- ToDoList.py
+++ ToDoList.py
@@ -39,10 +39,16 @@ class ToDoList:
     def edit_task(self):
         task_to_edit = int(input("Enter the task number to edit (like 1 for first task you added) : "))
         if task_to_edit < 1 or task_to_edit > len(self.incomplete_tasks):
-            raise ValueError("Task number invalid")
-        task_to_edit -= 1
-        updated_task = input("Enter the updated task you want in place of previous one : ")
-        self.incomplete_tasks[task_to_edit] = updated_task
+            print("Task number invalid")
+        else:
+            task_to_edit -= 1
+            updated_task = input("Enter the updated task you want in place of previous one : ").strip()
+            if len(updated_task):
+                self.incomplete_tasks[task_to_edit] = updated_task
+                print("Your task has been updated successfully!")
+            else:
+                print("Empty task")
+                print("Your task cannot be updated!")
 
     def mark_completed(self):
         self.view_tasks()
